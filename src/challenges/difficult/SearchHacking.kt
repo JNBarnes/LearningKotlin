8acae33a06105d83ba68751cdf9b59fc1a9a84ae
jbarnes <jbarnes@etsos.co.uk>
2016-06-06 10:48:23 +0100

    It works, but its slow and ugly. I can do better...

--- src/challenges/difficult/SearchHacking.kt
+++ src/challenges/difficult/SearchHacking.kt
@@ -7,24 +7,27 @@ import java.io.File
  * Created by jbarnes on 05/06/2016.
  * Challenge: https://www.reddit.com/r/dailyprogrammer/comments/47o4b6/20160226_challenge_255_hard_hacking_a_search/
  */
+val segmentTracker = SegmentTracker();
 
 fun main(args: Array<String>) {
-    var lines = File("C:\\oneliners.txt").readLines()
+    var lines = File("C:\\dump\\oneliners.txt").readLines()
+//    var lines = File("C:\\dump\\onelinerssmallx.txt").readLines()
+//    var lines = File("C:\\dump\\control.txt").readLines()
     var segmentedLines: MutableList<SegmentedLine> = mutableListOf()
-    val segmentTracker = SegmentTracker();
 
     for (i in lines.indices) {
         val stripped = stripPunctuationWhitespace(lines[i].toLowerCase())
         val segments = segmentString(stripped, 5)
-        segmentedLines.add(SegmentedLine(i, segments))
+        segmentedLines.add(SegmentedLine(i, segments, stripped))
     }
+
+    val commonSegments: MutableList<CommonSegment> = mutableListOf();
 
     for (i in segmentedLines.indices) { //for each line
         for (segment in segmentedLines[i].segments) { //check each segment
-            for (j in segmentedLines.indices) {//against every other lines' segments
-                if (i == j) continue;
-                if (segmentedLines[j].segments.contains(segment)) {
-                    segmentTracker.addSegment(segment, j)
+            for (segmentedLine in segmentedLines) { //against each other line's segments
+                if (segmentedLine.orig.contains(segment)) {
+                    segmentTracker.addSegment(segment, segmentedLine.lineNumber)
                 }
             }
         }
@@ -33,9 +36,8 @@ fun main(args: Array<String>) {
 
     val coveragelist = segmentTracker.getFullCoverage(lines.size);
     println(coveragelist.size)
-//    coveragelist.forEach { println(it.segment) }
+    coveragelist.sortedBy { it.segment }.forEach { println(it.segment) }
 }
-
 
 fun stripPunctuationWhitespace(text: String): String {
     val regex = Regex(pattern = """[^a-z]""")
@@ -44,7 +46,7 @@ fun stripPunctuationWhitespace(text: String): String {
 
 fun segmentString(string: String, segmentLength: Int): MutableSet<String> {
     val segments: MutableSet<String> = hashSetOf()
-    for (i in 0..string.length - (segmentLength + 1)) {
+    for (i in 0..string.length - (segmentLength)) {
         val segment = string.substring(i..i + segmentLength - 1)
         segments.add(segment)
     }
@@ -75,29 +77,39 @@ class SegmentTracker(val commonSegments: MutableList<CommonSegment> = mutableLis
         return commonSegments.filter { it.segment == segment }.size > 0
     }
 
-    fun getFullCoverage(lineCount: Int):List<CommonSegment> {
-        val coverage: BooleanArray = BooleanArray(lineCount)
+    fun getFullCoverage(lineCount: Int): List<CommonSegment> {
+        var coverage: BooleanArray = BooleanArray(lineCount)
+        var coverageBuffer: BooleanArray = BooleanArray(lineCount)
         coverage.fill(false)
-        commonSegments.removeAll { it.lineNumbers.size < 2 }
+        coverageBuffer.fill(false)
         commonSegments.sortBy { it.lineNumbers.size }
         commonSegments.reverse();
 
-        val segments :MutableList<CommonSegment> = mutableListOf();
+        val segments: MutableList<CommonSegment> = mutableListOf();
 
-       loop@for (i in commonSegments.indices){
-            for(lineNumber in commonSegments[i].lineNumbers){
-                coverage[lineNumber] = true;
+        loop@for (commonSegment in commonSegments) {
+            for (lineNumber in commonSegment.lineNumbers) {
+                coverageBuffer[lineNumber] = true;
             }
-           segments.add(commonSegments[i])
-           if (!coverage.contains(false)){
-               break@loop
-           }
+            if (coverageBuffer.count { it == true } > coverage.count { it == true }) {
+                coverage = coverageBuffer.copyOf()
+                segments.add(commonSegment)
+            } else {
+                coverageBuffer = coverage.copyOf()
+            }
+
+            if (coverage.count { it == true } == lineCount) {
+                break@loop;
+            }
+            val trueCount = coverage.count { it == true }
+            println("Coverage: $trueCount / $lineCount (${(trueCount.toDouble() / lineCount.toDouble()) * 100.00}%)")
+
         }
-        val trueCount = coverage.count {it == true}
-        println("Coverage: $trueCount / $lineCount (${(trueCount.toDouble()/lineCount.toDouble())*100.00}%)")
+        val trueCount = coverage.count { it == true }
+        println("Coverage: $trueCount / $lineCount (${(trueCount.toDouble() / lineCount.toDouble()) * 100.00}%)")
         return segments;
     }
 }
 
-data class SegmentedLine(val lineNumber: Int, val segments: MutableSet<String>)
+data class SegmentedLine(val lineNumber: Int, val segments: MutableSet<String>, val orig: String)
 data class CommonSegment(val segment: String, val lineNumbers: MutableSet<Int> = mutableSetOf())
